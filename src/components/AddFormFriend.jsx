370d9b065f9b77ec7d6ad65719be1db54a170cc0
roshana01 <roshanaroshani99@gmail.com>
2024-01-10 10:54:09 +0330

    Add newDataFrind

--- src/components/AddFormFriend.jsx
+++ src/components/AddFormFriend.jsx
@@ -1,13 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 
-export default function AddFormFriend() {
+export default function AddFormFriend({ addFrirndFun }) {
+  //satate in input take data
+  const [name, setName] = useState("");
+  const [avatar, setAvatar] = useState("./public/image/avatar (3).png");
+  //function onsubmit in the form
+  const onSubmitHandler = (e) => {
+    if (!name || !avatar) return;
+    e.preventDefault();
+
+    const newDataFriend = {
+      name,
+      avatar,
+      balance: 3,
+      id: crypto.randomUUID(),
+    };
+
+    addFrirndFun(newDataFriend);
+    setName("");
+  };
+
   return (
     <>
-      <form className="form-add-friend">
+      <form className="form-add-friend" onSubmit={onSubmitHandler}>
         <label>💑Friend Name</label>
-        <input type="text" name="" id="" />
+        <input
+          type="text"
+          name=""
+          id=""
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
         <label>🌌Image URL </label>
-        <input type="text" name="" id="" placeholder="" />
+        <input
+          type="text"
+          name=""
+          id=""
+          placeholder=""
+          value={avatar}
+          onChange={(e) => setAvatar(e.target.value)}
+        />
         <button className="button">Add</button>
         {/* <Button>Add</Button> */}
       </form>
