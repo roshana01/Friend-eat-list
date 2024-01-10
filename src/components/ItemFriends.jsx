5997ad5a27f13a57c9ee0742f58619ade3bbe8d5
roshana01 <roshanaroshani99@gmail.com>
2024-01-10 09:56:52 +0330

    Add : style and html for items friend

--- src/components/ItemFriends.jsx
+++ src/components/ItemFriends.jsx
@@ -1,7 +1,58 @@
-import React from 'react'
+import React from "react";
 
 export default function ItemFriends() {
   return (
-    <div>ItemFriends</div>
-  )
+    <>
+      <li className="select">
+        <img src="./public/image/avatar (1).png" alt="friend" className="img" />
+        <h3>deli🥑</h3>
+        <p className="red">You own -7$</p>
+        {/* <p className="red">
+          You own {friends.name} {Math.abs(friends.balance)}$
+        </p>
+
+        <p className="green">
+          {friends.name} owes you {friends.balance}$
+        </p>
+
+        <p>You and {friends.name} even</p> */}
+
+        <button className="button">Select</button>
+      </li>
+      <li className="select">
+        <img src="./public/image/avatar (2).png" alt="friend" className="img" />
+        <h3>milad🥑</h3>
+        <p className="green">You own 23$</p>
+
+        {/* <p className="red">
+          You own {friends.name} {Math.abs(friends.balance)}$
+        </p>
+
+        <p className="green">
+          {friends.name} owes you {friends.balance}$
+        </p>
+
+        <p>You and {friends.name} even</p> */}
+
+        <button className="button">Select</button>
+      </li>
+      <li className="select">
+        <img src="./public/image/avatar (3).png" alt="friend" className="img" />
+        <h3>asra🥑</h3>
+        <p className="">You own 12$</p>
+
+        {/* <p className="red">
+          You own {friends.name} {Math.abs(friends.balance)}$
+        </p>
+
+        <p className="green">
+          {friends.name} owes you {friends.balance}$
+        </p>
+
+        <p>You and {friends.name} even</p> */}
+
+        <button className="button">Select</button>
+      </li>
+    </>
+  );
 }
--- src/components/ListFriends.jsx
+++ src/components/ListFriends.jsx
@@ -1,7 +1,14 @@
-import React from 'react'
+import React from "react";
+import ItemFriends from "./ItemFriends";
 
 export default function ListFriends() {
   return (
-    <div>ListFriends</div>
-  )
+    <ul>
+      {/* {addListFriend.map((friend) => (
+        //components items
+        <ItemFriends />
+      ))} */}
+      <ItemFriends/>
+    </ul>
+  );
 }
