a751a9fb3404803e87018f18a9c4c4d9b60b516e
roshana01 <roshanaroshani99@gmail.com>
2024-01-10 10:54:27 +0330

    add state dataFriend

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import AddFormFriend from "./AddFormFriend";
 import ListFriends from "./ListFriends";
 import ListBill from "./ListBill";
-
+import FrindeData from "../assets/data";
+// FrindeData
 export default function App() {
+  const [newFriend, setNewFriend] = useState(FrindeData);
+
+  const addNewFriendHandler = (friend) => {
+    setNewFriend((fri) => [...fri, friend]);
+  };
+
   return (
     <>
       <div className="app">
         <div className="sidebar">
-          <ListFriends />
-          <AddFormFriend />
+          <ListFriends newFriend={newFriend}/>
+          <AddFormFriend addFrirndFun={addNewFriendHandler}/>
         </div>
         <ListBill />
       </div>
