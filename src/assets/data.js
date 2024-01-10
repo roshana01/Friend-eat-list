3affaa51daaff8eb7568ade941f368bc4c69a36f
roshana01 <roshanaroshani99@gmail.com>
2024-01-10 09:54:39 +0330

    Add : data Friends

---
+++ src/assets/data.js
@@ -0,0 +1,20 @@
+const FrindeData = [
+  {
+    name: 'roshanak',
+    avatar: '',
+    id: 2345
+  },
+  {
+    name: 'milad',
+    avatar: '',
+    id: 2346
+  },
+  {
+    name: 'delaram',
+    avatar: '',
+    id: 2347
+  },
+
+]
+
+export default FrindeData;
