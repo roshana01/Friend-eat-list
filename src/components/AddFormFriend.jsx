3ced4a3eb85e721e6a61a55cdcb05196af9dbff8
roshana01 <roshanaroshani99@gmail.com>
2024-01-10 09:55:41 +0330

    Add : code html for desighn

--- src/components/AddFormFriend.jsx
+++ src/components/AddFormFriend.jsx
@@ -1,7 +1,16 @@
-import React from 'react'
+import React from "react";
 
 export default function AddFormFriend() {
   return (
-    <div>AddFormFriend</div>
-  )
+    <>
+      <form className="form-add-friend">
+        <label>💑Friend Name</label>
+        <input type="text" name="" id="" />
+        <label>🌌Image URL </label>
+        <input type="text" name="" id="" placeholder="" />
+        <button className="button">Add</button>
+        {/* <Button>Add</Button> */}
+      </form>
+    </>
+  );
 }
