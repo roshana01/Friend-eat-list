9121930349b684c4c3bd0adf331f7f78f7f1c8cb
roshana01 <roshanaroshani99@gmail.com>
2024-01-10 09:57:30 +0330

    Add maked styles and html at components

--- src/components/ListBill.jsx
+++ src/components/ListBill.jsx
@@ -1,7 +1,27 @@
-import React from 'react'
+import React from "react";
 
 export default function ListBill() {
   return (
-    <div>ListBill</div>
-  )
+    <>
+      <form action="" className="form-split-bill" >
+        <h2>Split a bill with sara</h2>
+        <label>💰Bill value</label>
+        <input type="number" />
+
+        <label>🧍‍♀️You expenase</label>
+        <input type="number" />
+
+        <label>💑X's expenase</label>
+        <input type="number" name="" id="" disabled />
+
+        <label>🤑How is paying the bill?</label>
+        <select name="" id="">
+          <option value={""}>You</option>
+          <option value="friend">sara</option>
+        </select>
+
+        <button className="button">Split Bill</button>
+      </form>
+    </>
+  );
 }
