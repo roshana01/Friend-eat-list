0366eeb6e28b85baa16d0eb3177adeca0df03ea9
roshana01 <roshanaroshani99@gmail.com>
2024-01-10 11:14:04 +0330

    pushed components Button and add function show form friend

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,9 +3,16 @@ import AddFormFriend from "./AddFormFriend";
 import ListFriends from "./ListFriends";
 import ListBill from "./ListBill";
 import FrindeData from "../assets/data";
+import Button from "./Button";
+
 // FrindeData
 export default function App() {
   const [newFriend, setNewFriend] = useState(FrindeData);
+  const [showFriend, setShowFriend] = useState(false);
+
+  const showFrindHandler = () => {
+    setShowFriend((show) => !show);
+  };
 
   const addNewFriendHandler = (friend) => {
     setNewFriend((fri) => [...fri, friend]);
@@ -15,8 +22,11 @@ export default function App() {
     <>
       <div className="app">
         <div className="sidebar">
-          <ListFriends newFriend={newFriend}/>
-          <AddFormFriend addFrirndFun={addNewFriendHandler}/>
+          <ListFriends newFriend={newFriend} />
+          {showFriend && <AddFormFriend addFrirndFun={addNewFriendHandler} />}
+          <Button onClick={showFrindHandler}>
+            {showFriend ? "Closed" : "AddFriend"}
+          </Button>
         </div>
         <ListBill />
       </div>
